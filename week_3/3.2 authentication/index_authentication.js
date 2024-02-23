9e60257ef37401728b574277ac5e5f6fdb6881e0
ashusoni24 <24ashusoni@gmail.com>
2024-02-23 14:20:27 +0530

    adds

---
+++ week_3/3.2 authentication/index_authentication.js
@@ -0,0 +1,68 @@
+//concepts:-
+//1. hashing :- converting some simple string into some complecated gebrish lang
+          //such that the same string has always that gebrish lang
+          //and cannot convert back or make asumtions for pass.
+//2.encryption :-can convert back using key
+//3.json web tokens :-  1.  json - takes json as a input
+                    //  2. tokesn - takes input and gives back us a long string
+                    //anyone having the output can see your input
+
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const jwtPassword = "123456";
+
+const app = express();
+
+const ALL_USERS = [
+  {
+    username: "harkirat@gmail.com",
+    password: "123",
+    name: "harkirat singh",
+  },
+  {
+    username: "raman@gmail.com",
+    password: "123321",
+    name: "Raman singh",
+  },
+  {
+    username: "priya@gmail.com",
+    password: "123321",
+    name: "Priya kumari",
+  },
+];
+
+function userExists(username, password) {
+  // write logic to return true or false if this user exists
+  // in ALL_USERS array
+}
+
+app.post("/signin", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!userExists(username, password)) {
+    return res.status(403).json({
+      msg: "User doesnt exist in our in memory db",
+    });
+  }
+
+  var token = jwt.sign({ username: username }, "shhhhh");
+  return res.json({
+    token,
+  });
+});
+
+app.get("/users", function (req, res) {
+  const token = req.headers.authorization;
+  try {
+    const decoded = jwt.verify(token, jwtPassword);
+    const username = decoded.username;
+    // return a list of users other than this username
+  } catch (err) {
+    return res.status(403).json({
+      msg: "Invalid token",
+    });
+  }
+});
+
+app.listen(3000)
