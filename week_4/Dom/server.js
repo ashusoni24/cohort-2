da916292731333cd56df185b8314a6335403b2c6
ashusoni24 <24ashusoni@gmail.com>
2024-03-12 22:20:57 +0530

    dom manipulation

--- week_4/Dom/server.js
+++ week_4/Dom/server.js
@@ -1,6 +1,12 @@
 const express= require("express")
 const app = express()
 const port =3000
+//handling cors errors -- this allows the request from any frontend to make req on this server and take response
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 app.get("/sum", function(req,res){
     const a= parseInt(req.query.a);
     const b = parseInt(req.query.b);
